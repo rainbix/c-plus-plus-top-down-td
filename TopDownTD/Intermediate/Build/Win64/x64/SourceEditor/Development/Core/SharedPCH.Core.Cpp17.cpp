cccb91f113033c8edbd5d67eaf5112ed394099ff
vzlomvl <vzlomvl@gmail.com>
2024-03-27 12:32:40 +0200

    Updated gitignore. Remote ignored files. Upload gitattributes.

--- TopDownTD/Intermediate/Build/Win64/x64/SourceEditor/Development/Core/SharedPCH.Core.Cpp17.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32545
-#include "D:/Projects/c-plus-plus-top-down-td/Source/Intermediate/Build/Win64/x64/SourceEditor/Development/Core/SharedPCH.Core.Cpp17.h"
--- TopDownTD/Intermediate/Build/Win64/x64/SourceEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Projects/c-plus-plus-top-down-td/Source/Intermediate/Build/Win64/x64/SourceEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
