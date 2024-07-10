dab8b69872920cb6ad12b2eda2da4fc0598d7a4f
elvess65 <elvess65@gmail.com>
2024-07-10 12:50:30 +0300

    Fix for broken data

--- TopDownTD/Source/Source/Tools/Interpolator.h
+++ TopDownTD/Source/Source/Tools/Interpolator.h
@@ -15,4 +15,23 @@ public:
 	T Lerp(T (*lerp_func)(const T& f, const T& t, const float d));
 };
 
+// The separation of the template class definition and its member function implementations into different files
+//resulted in files not being compiled now and then.
+// In C++, template definitions and their implementations must be in the same file.
+// This is because the compiler needs to see the full definition of the template in order to generate code for each instantiation of the template.
+
+template <class T>
+Interpolator<T>::Interpolator(float totalTime, T from, T to): SimpleInterpolator(totalTime)
+{
+	From = from;
+	To = to;
+}
+
+template <class T>
+T Interpolator<T>::Lerp(T(* lerp_func)(const T& f, const T& t, const float d))
+{
+	return lerp_func(From, To, Progress());
+}
+
+
 
