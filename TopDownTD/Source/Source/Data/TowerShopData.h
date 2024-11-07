02707a220dd94d832018bea8f3c6e4ef19b92dea
STORM404\pavvl <pvladislavua@gmail.com>
2024-11-07 13:21:28 +0200

    [*] Merge

---
+++ TopDownTD/Source/Source/Data/TowerShopData.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "TowerShopData.generated.h"
+
+class ATowerActor;
+
+USTRUCT(BlueprintType)
+struct SOURCE_API FTowerShopData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	FText TowerName;
+
+	UPROPERTY(EditDefaultsOnly)
+	int BuildTime;
+
+	UPROPERTY(EditDefaultsOnly)
+	int Cost;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UTexture2D> TowerIcon;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ATowerActor> TowerClass;
+};
