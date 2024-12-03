b11d07636735298e4989a87ab5a8f597847ff941
mkislenko <mkislenko@gmail.com>
2024-12-03 13:14:53 +0200

    [Add] clip ammo with infinite spare

---
+++ TopDownTD/Source/Source/Weapons/AmmoModule/ClipAmmoInfiniteSpareAmmoModule.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClipAmmoInfiniteSpareAmmoModule.h"
+
+void UClipAmmoInfiniteSpareAmmoModule::Initialize()
+{
+	Super::Initialize();
+
+	CurrentClipAmmo = MaxClipAmmo;
+}
+
+bool UClipAmmoInfiniteSpareAmmoModule::CheckShootCost() const
+{
+	return CurrentClipAmmo > 0;
+}
+
+void UClipAmmoInfiniteSpareAmmoModule::ApplyShootCost()
+{
+	CurrentClipAmmo--;
+
+	OnAmmoChanged.Execute();
+}
+
+int UClipAmmoInfiniteSpareAmmoModule::GetCurrentAmmo() const
+{
+	return CurrentClipAmmo;
+}
+
+int UClipAmmoInfiniteSpareAmmoModule::GetSpareAmmo() const
+{
+	return MaxClipAmmo;
+}
+
+bool UClipAmmoInfiniteSpareAmmoModule::CanReload() const
+{
+	return CurrentClipAmmo < MaxClipAmmo;
+}
+
+void UClipAmmoInfiniteSpareAmmoModule::ApplyReload()
+{
+	CurrentClipAmmo = MaxClipAmmo;
+
+	OnAmmoChanged.Execute();
+}
+
+
+
---
+++ TopDownTD/Source/Source/Weapons/AmmoModule/ClipAmmoInfiniteSpareAmmoModule.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InfiniteAmmoModule.h"
+#include "UObject/Object.h"
+#include "ClipAmmoInfiniteSpareAmmoModule.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOURCE_API UClipAmmoInfiniteSpareAmmoModule : public UInfiniteAmmoModule
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Parameters")
+	int MaxClipAmmo = 10;
+
+	int CurrentClipAmmo;
+public:
+	virtual void Initialize() override;
+	virtual bool CheckShootCost() const override;
+	virtual void ApplyShootCost() override;
+	virtual int GetCurrentAmmo() const override;
+	virtual int GetSpareAmmo() const override;
+	virtual bool CanReload() const override;
+	virtual void ApplyReload() override;
+};
